669605dc8f608d53d69aaa2e5960af4f5c02e696
Francis Kingori <Francis.Kingori@tovutigroup.com>
2021-06-20 11:10:35 +0300

    update

---
+++ Sleeqcarhire/Controllers/CustomersController.cs
@@ -0,0 +1,26 @@
+﻿using DBL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sleeqcarhire.Controllers
+{
+    [Authorize]
+    public class CustomersController : BaseController
+    {
+        private readonly BL bl;
+        string encusercode = "";
+        public CustomersController()
+        {
+            bl = new BL(Util.GetDbConnString());
+        }
+        [HttpGet]
+        public IActionResult Vehicleownerslist()
+        {
+            return View();
+        }
+    }
+}
