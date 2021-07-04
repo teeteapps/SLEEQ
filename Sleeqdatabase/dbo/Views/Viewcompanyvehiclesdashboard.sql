f95fc8b2195349ae453f83468d6d5d3bbb051837
Francis Kingori <Francis.Kingori@tovutigroup.com>
2021-07-04 22:14:45 +0300

    update

---
+++ Sleeqdatabase/dbo/Views/Viewcompanyvehiclesdashboard.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW Viewcompanyvehiclesdashboard AS SELECT CV.Typecode,CV.Typename,
+CP.Capname,
+CV.Imagepath,CASE WHEN CV.Cartype=100 THEN 'Salon' ELSE 'Executive' END AS Cartype,HT.Hireday,HT.Hireprice
+FROM Compvehicletypes CV 
+INNER JOIN Vehicletypehireterms HT ON CV.Typecode=HT.Typecode
+INNER JOIN Capacity CP ON CP.Capcode=CV.Capacity
