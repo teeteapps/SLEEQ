1a8e3045dc5d7faf4351f3bf23a7c0a17bd9f034
user <franciskingori448@gmail.com>
2021-09-25 10:36:11 +0300

    update

---
+++ Sleeqcarhire/Helpers/Azurestoragefileupload.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sleeqcarhire.Helpers
+{
+    public class Azurestoragefileupload
+    {
+    }
+}
